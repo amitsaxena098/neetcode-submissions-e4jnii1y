332433e45d336cf69780cb4b3dbc496272775e36
Amit Saxena <48174393+amitsaxena098@users.noreply.github.com>
2026-04-06 00:38:03 +0530

    Add: merge-intervals - submission-3

---
+++ Data Structures & Algorithms/merge-intervals/submission-3.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((first, second) -> {
+            if(first[0] != second[0]) {
+                return Integer.compare(first[0], second[0]);
+            }
+            return Integer.compare(first[1], second[1]);
+        });
+
+        for(int[] interval : intervals) {
+            pq.offer(interval);
+        }
+
+        List<int[]> res = new ArrayList<>();
+        int count = 0;
+        while(!pq.isEmpty()) {
+            int[] currentInterval = pq.poll();
+            System.out.println(currentInterval[0] + ", " + currentInterval[1]);
+
+            while(!pq.isEmpty() && currentInterval[1] >= pq.peek()[0]) {
+                currentInterval[1] = Math.max(pq.peek()[1], currentInterval[1]);
+                pq.poll();
+            }
+
+            res.add(currentInterval);
+
+        }
+        return res.toArray(new int[res.size()][]);
+        
+    }
+}
